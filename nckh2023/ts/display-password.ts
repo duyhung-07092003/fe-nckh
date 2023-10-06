5c31978ed77bb67f38cc7670f780f665058e12d7
duyhung-07092003 <101647542+duyhung-07092003@users.noreply.github.com>
2023-10-06 19:43:44 +0700

    Delete nckh2023 directory

--- nckh2023/ts/display-password.ts
+++
@@ -1,7 +0,0 @@
-import { querySelector, addEventListener } from "dom";
-
-export function displayPassword(eyeIcon: HTMLElement, passwordInput: HTMLElement) {
-  addEventListener(eyeIcon, "click", () => {
-    passwordInput.setAttribute("type", passwordInput.getAttribute("type") === "password" ? "text" : "password");
-  });
-}
